a0160999da0d6da4e83671e16b652301d4b11aca
fiffi95 <sofiajacobsson@live.se>
2017-02-14 13:43:58 +0100

    all pages merged, OK

--- lab3/js/view/view1.js
+++ lab3/js/view/view1.js
@@ -4,11 +4,7 @@ var View1 = function (container, model) {
 	model.addObserver(this);
 	
 	// Get all the relevant elements of the view (ones that show data and/or ones that responed to interaction)
-	this.view6NumberOfGuests = container.find("#view6NumberOfGuests");
-
-	this.view6NumberOfGuests.html('My Dinner: ' + model.getNumberOfGuests() + ' people');
 
 	this.update = function () {
-		this.view6NumberOfGuests.html('My Dinner: ' + model.getNumberOfGuests() + ' people');
 	}
 }
