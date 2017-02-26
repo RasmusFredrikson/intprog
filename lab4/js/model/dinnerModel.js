78af7e7835c4533aa24c8610ba374acb3f24874f
fiffi95 <sofiajacobsson@live.se>
2017-02-26 15:41:20 +0100

    error handling, OK

--- lab4/js/model/dinnerModel.js
+++ lab4/js/model/dinnerModel.js
@@ -142,9 +142,25 @@ var DinnerModel = function() {
 				console.log(data);
 				cb(data);
 			},
-			error: function(data) {
-				console.log("Error!")
-				console.log(data);
+			error: function (jqXHR, exception) {
+				console.log(jqXHR.responseText);
+				var msg = '';
+				if (jqXHR.status === 0) {
+					msg = 'Not connected.\n Verify Network.';
+				} else if (jqXHR.status == 404) {
+					msg = 'Requested page not found. [404]';
+				} else if (jqXHR.status == 500) {
+					msg = 'Internal Server Error [500].';
+				} else if (exception === 'parsererror') {
+					msg = 'Requested JSON parse failed.';
+				} else if (exception === 'timeout') {
+					msg = 'Time out error.';
+				} else if (exception === 'abort') {
+					msg = 'Ajax request aborted.';
+				} else {
+					msg = 'Uncaught Error.\n' + jqXHR.responseText;
+				}
+				alert(msg);
 			}
 		});
 	}
@@ -160,10 +176,24 @@ var DinnerModel = function() {
 			success: function(data) {
 				cb(data);
 			},
-			error: function(data) {
-				console.log("Error!")
-				console.log(data);
-
+			error: function (jqXHR, exception) {
+				var msg = '';
+				if (jqXHR.status === 0) {
+					msg = 'Not connected.\n Verify Network.';
+				} else if (jqXHR.status == 404) {
+					msg = 'Requested page not found. [404]';
+				} else if (jqXHR.status == 500) {
+					msg = 'Internal Server Error [500].';
+				} else if (exception === 'parsererror') {
+					msg = 'Requested JSON parse failed.';
+				} else if (exception === 'timeout') {
+					msg = 'Time out error.';
+				} else if (exception === 'abort') {
+					msg = 'Ajax request aborted.';
+				} else {
+					msg = 'Uncaught Error.\n' + jqXHR.responseText;
+				}
+				alert(msg);
 			}
 		});
 	}
