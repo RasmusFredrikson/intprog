21925a98cf94fd08f7d68887e22e3072eb74c4a2
fiffi95 <sofiajacobsson@live.se>
2017-03-03 16:50:30 +0100

    added background image, on view1

--- lab5/app/js/searchCtrl.js
+++ lab5/app/js/searchCtrl.js
@@ -3,6 +3,10 @@
 dinnerPlannerApp.controller('SearchCtrl', function ($scope,Dinner) {
 	// TODO in Lab 5: you will need to implement a method that searchers for dishes
 	// including the case while the search is still running.
+
+	document.getElementsByTagName("body")[0].className = "";
+//	body.classList.remove("backgroundImg");
+
 	$scope.search = function(query,type) {
 		$scope.status = "Searching...";
 		Dinner.DishSearch.get({query:query,type:type},function(data){
